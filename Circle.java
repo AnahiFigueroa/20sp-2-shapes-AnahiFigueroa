f64879e82b74e29fef88370858ed67075769cc48
AnahiFigueroa <af1009@georgetown.edu>
2020-02-19 08:33:39 -0500

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,34 +1,30 @@
 import java.lang.Math.*;
 import java.util.Vector;
 
+
 public class Circle extends Shape {
-
-	public double radius;
-	public double area;
-	public double perimeter;
-
-	public double getArea() {
-		return area;
-	}
-
-
-	public double getPerimeter() {
-		return perimeter;
-	}
-
-
-	public void SetCircleArea(){
- 	 double area = Math.PI * (radius * radius);
-  }
-
-   public void SetCirclePerimeter(){
- 		double perimeter = 2 * (Math.PI * radius);
- 	}
-
+	
 	public Circle(ShapeDescription description) {
 		super(description);
 		Vector<Double> newvector = description.getDoubles();
 		radius = newvector.get(0);
 
 	}
+
+	public double radius;
+	public double area;
+	public double perimeter;
+
+	public double getArea() {
+		double area = Math.PI * (radius * radius);
+		return area;
+	}
+
+
+	public double getPerimeter() {
+		double perimeter = 2 * (Math.PI * radius);
+		return perimeter;
+ 	}
+	
+
 }
