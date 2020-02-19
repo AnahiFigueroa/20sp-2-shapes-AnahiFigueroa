8afc0d2965e8644d675b445177e8985275685ec1
AnahiFigueroa <af1009@georgetown.edu>
2020-02-19 00:26:41 -0500

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -7,17 +7,17 @@ public abstract class Shape {
 	 * @param description the name of the shape and some doubles that define it
 	 */
 	Shape( ShapeDescription description ) {}
-	
+
 	/**
 	 * Returns the area of a shape
 	 * @return the area
 	 */
 	public abstract double getArea();
-	
-	
+
+
 	/**
 	 * Returns the perimeter of a shape
-	 * @return the perimeter 
+	 * @return the perimeter
 	 */
 	public abstract double getPerimeter();
 }
