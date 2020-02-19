e6e7b917eb2298faa92b6445fcd0ecce241fa4f0
AnahiFigueroa <af1009@georgetown.edu>
2020-02-19 08:33:47 -0500

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,14 +1,23 @@
 import java.lang.Math.*;
 import java.util.Vector;
 
-
-
 public class Triangle extends Shape{
 
+	public Triangle(ShapeDescription description) {
+		super(description);
+		Vector<Double> newvector = description.getDoubles();
+		Double sideA = newvector.get(0);
+		Double sideB = newvector.get(1);
+		Double sideC = newvector.get(2);
+	}
 
-	int sideA;
-	int sideB;
-	int sideC;
+	
+	Double sideA;
+	Double sideB;
+	Double sideC;
+	
+	
+
 	public double getArea() {
 		double s = (getPerimeter()/2);
 		double area = Math.sqrt(s*(s-sideA)* (s-sideB) * (s-sideC));
@@ -17,19 +26,11 @@ public class Triangle extends Shape{
 	}
 	public double getPerimeter() {
 		double perimeter = sideA + sideB + sideC;
-		returns perimeter;
+		return perimeter;
 	}
 
-
-	public Trangle(ShapeDescription description) {
-		super(description);
-		Vector<Double> newvector = description.getDoubles();
-		sideA = newvector.get(0);
-		sideB = newvector.get(1);
-		sideC = newvector.get(2);
-	}
 }
-
+	
 
 //remove abstract from function names and change class header to "extends shape"
 //change function names to be the same as the shape class
