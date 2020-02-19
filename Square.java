94f36b946958024ed4f94305e44e849ef76d0b6c
AnahiFigueroa <af1009@georgetown.edu>
2020-02-19 00:26:50 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -3,31 +3,30 @@ import java.util.Vector;
 
 
 public class Square extends Shape {
-	
-	int sideLength; 
-	
+
+	int sideLength;
+
 	public double getArea() {
-		
-		double area = sideLength * sideLength; 
-		
-		returns area; 
-		
-	}
-	public double getPerimeter() {
-		
-		double perimeter = 2 * (sidelength + sidelength); 
-		
-		returns perimeter; 
+
+		double area = sideLength * sideLength;
+
+		returns area;
 
 	}
-	
-	
-	public Square(ShapeDescription description) {
-		super(description); 
-		Vector<Double> newvector = description.getDoubles(); 
-		sideLength = newvector.get(0); 
-	
+	public double getPerimeter() {
+
+		double perimeter = 2 * (sidelength + sidelength);
+
+		returns perimeter;
+
 	}
-}
-	  
+
+
+	public Square(ShapeDescription description) {
+		super(description);
+		Vector<Double> newvector = description.getDoubles();
+		sideLength = newvector.get(0);
+
+	}
+
 }
