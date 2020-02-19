ccdc33e55587172ffc3b090f594fc97db49ef2a2
AnahiFigueroa <af1009@georgetown.edu>
2020-02-19 08:33:35 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,28 +1,8 @@
 import java.util.Vector;
 
+
 public class Rectangle extends Shape{
-
-	int width;
-	int height;
-
-	public double getArea() {
-
-		double area = 0;
-		area = width * height;
-
-
-		returns area;
-
-	}
-	public double getPerimeter() {
-
-		double perimeter = 0;
-		perimeter = 2 * (width + height);
-
-
-		returns perimeter;
-	}
-
+	
 	public Rectangle(ShapeDescription description) {
 		super(description);
 		Vector<Double> newvector = description.getDoubles();
@@ -30,5 +10,24 @@ public class Rectangle extends Shape{
 		height = newvector.get(1);
 
 	}
+
+	Double width;
+	Double height;
+
+	public double getArea() {
+
+		double area = 0;
+		area = width * height;
+		return area;
+
+	}
+	public double getPerimeter() {
+
+		double perimeter = 0;
+		perimeter = 2 * (width + height);
+		return perimeter;
+	}
+
+	
 
 }
